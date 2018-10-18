c8207b830ffa84763716622498bcdee66ca7a5d4
gyjlovelh <18062791691@163.com>
2018-10-18 15:57:05 +0800

    auto commit

--- ci-scripts/build.sh
+++ ci-scripts/build.sh
@@ -1,53 +1,55 @@
 #!/usr/bin/env bash
 
 # 执行netease-theme的build脚本
-path = ~/Documents/angular6
-bash $path/netease-theme/ci-scripts/build.sh
+set pathName = ~/Documents/angular6
+echo "this is path:" ${pathName}
+bash  ~/Documents/angular6/netease-theme/ci-scripts/build.sh
 
-cd $path/netease-common
-# 升级netease-theme工程依赖
-npm run upgrade
-if [ $? == 0 ]
-then echo "升级theme版本成功!"
-else
-echo "升级theme版本失败!"
-exit
-fi
-
-# 安装依赖
-# yarn
+#
+#cd $path/netease-common
+## 升级netease-theme工程依赖
+#npm run upgrade
 #if [ $? == 0 ]
-#then echo "安装成功!"
+#then echo "升级theme版本成功!"
 #else
-#git commit -m 'auto commit'
-#git push origin master
+#echo "升级theme版本失败!"
+#exit
 #fi
-
-# 升级版本
-npm version patch
-if [ $? == 0 ]
-then echo "版本升级成功!"
-else
-git commit -a -m "neteae-common auto commit"
-echo "neteae-common auto commit!"
-fi
-
-# 打包
-npm run package
-if [ $? == 0 ]
-then echo "打包成功!"
-else
-echo "npm run package failed!"
-exit
-fi
-
-# 发布
-npm publish dist
-if [ $? == 0 ]
-then echo "发布成功!"
-else
-echo "npm publish dist failed!"
-exit
-fi
+#
+## 安装依赖
+## yarn
+##if [ $? == 0 ]
+##then echo "安装成功!"
+##else
+##git commit -m 'auto commit'
+##git push origin master
+##fi
+#
+## 升级版本
+#npm version patch
+#if [ $? == 0 ]
+#then echo "版本升级成功!"
+#else
+#git commit -a -m "neteae-common auto commit"
+#echo "neteae-common auto commit!"
+#fi
+#
+## 打包
+#npm run package
+#if [ $? == 0 ]
+#then echo "打包成功!"
+#else
+#echo "npm run package failed!"
+#exit
+#fi
+#
+## 发布
+#npm publish dist
+#if [ $? == 0 ]
+#then echo "发布成功!"
+#else
+#echo "npm publish dist failed!"
+#exit
+#fi
 
 
